9eb3b9112efcb6e512df361b4d1931c05422a1e7
Rui <rui.minetest@gmail.com>
2016-10-29 11:19:07 +0900

    Update init.lua

--- yamato_decors/init.lua
+++ yamato_decors/init.lua
@@ -2,4 +2,5 @@ local modpath = minetest.get_modpath("yamato_decors")
 
 dofile(modpath .. "/andon.lua")
 dofile(modpath .. "/tatami.lua")
+dofile(modpath .. "/tobiishi.lua")
 dofile(modpath .. "/wadansu.lua")
